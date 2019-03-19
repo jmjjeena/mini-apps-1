be86d951e989a7537d840892c7c9f136774a5a38
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-03-18 20:44:58 -0700

    Created the box and the button

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,6 @@
+var table = document.getElementById('table');
+var box = document.getElementsByClassName('cell');
 
-
+box.addEventListener('click', event => {
+  box.innerHTML = `X ${event.detail}`;
+});
