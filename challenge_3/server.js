8ef5c45343f5bad938213abb1991369061b33e5b
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-03-20 15:44:33 -0700

    Implement Get request

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// initialize our express app
+var app = express();
+var port = 7536;
 
 
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://127.0.0.1:27017/jmjdb' , function(err, database) {
+    if(err) {
+      throw err;
+    }
+    db = database.db('jmjdb');
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+});
+
+
+app.get('/', function(req, res) {
+    db.collection("users").find({}).toArray(function(err, data) {
+        if(data) {
+            console.log(data);
+            res.send(data);
+        } else {
+            res.end();
+        }
+    })
+})
+
+// app.post('/', function(req, res) {
+//     db.coll
+// })
