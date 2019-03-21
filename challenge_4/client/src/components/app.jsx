c16e7bddc930bd5d1b113645d26306455c0677cf
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-03-21 12:17:48 -0700

    Able to render from App component

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App  extends React.Component {
+  render() {
+    return (
+      <h1>Hello,world</h1>
+    )
+  }
+}
 
+export default App;
